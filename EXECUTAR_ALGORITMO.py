309d0e686ec73a71e5e32f25f11418899cf328df
carlosdanielpohlod <carlospohlod@gmail.com>
2021-06-28 20:40:04 -0300

    arrumado o array

--- EXECUTAR_ALGORITMO.py
+++ EXECUTAR_ALGORITMO.py
@@ -2,10 +2,27 @@ from hiperHeuristica_Thompson import *
 # from Heuristicas_Estaticas import heuristica_estatica
 from hiperHeristica_Aleatoria import *
 arquivo = ArquivosManager()
-nomes =  [
+nomes = [
+    '/content/HiperHeuristic/storage/files/nug/nug12.dat',
+    '/content/HiperHeuristic/storage/files/nug/nug14.dat',
+    '/content/HiperHeuristic/storage/files/nug/nug15.dat',
+    '/content/HiperHeuristic/storage/files/nug/nug16a.dat',
+    '/content/HiperHeuristic/storage/files/nug/nug16b.dat',
+    '/content/HiperHeuristic/storage/files/nug/nug17.dat',
+    '/content/HiperHeuristic/storage/files/nug/nug18.dat',
 
-    'storage/files/nug/nug16b.dat',
-    
+'/content/HiperHeuristic/storage/files/nug/nug20.dat',
+
+'/content/HiperHeuristic/storage/files/nug/nug21.dat',
+'/content/HiperHeuristic/storage/files/nug/nug22.dat',
+'/content/HiperHeuristic/storage/files/nug/nug25.dat',
+'/content/HiperHeuristic/storage/files/nug/nug27.dat',
+
+
+'/content/HiperHeuristic/storage/files/nug/nug28.dat'
+
+
+
 ]
 for nome in nomes:
 
